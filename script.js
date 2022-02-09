6c83e20d2cf25bbe7ab88c4b58991404c617f1c8
EdgarGrcs <edgar.gricius2@gmail.com>
2022-02-09 20:40:29 +0100

    add grid, functions

---
+++ script.js
@@ -0,0 +1,42 @@
+// Sets important constants and variables
+
+const container = document.getElementById("container");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
+
+// Creates a default grid sized 16x16
+function defaultGrid() {
+    makeRows(16);
+    makeColumns(16);
+}
+
+// Takes (rows, columns) input and makes a grid
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+      let cell = document.createElement("div");
+
+      cell.addEventListener("click", () => {
+        cell.style.setProperty("background-color", "black");
+    });
+      
+      container.appendChild(cell).className = "grid-item";
+    };
+  };
+
+// Creates columns
+function makeColumns(cellNum) {
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < cellNum; j++) {
+            let newCell = document.createElement("div");
+            rows[j].appendChild(newCell).className = "cell";
+        };
+
+    };
+};
+
+makeRows(8, 8);
+
+
+
