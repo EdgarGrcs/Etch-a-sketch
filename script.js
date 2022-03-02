b5b78ec3ccac6a8d25802f5cdefabac83302675e
EdgarGrcs <edgar.gricius2@gmail.com>
2022-03-02 22:48:42 +0100

    remove css stuff

--- script.js
+++ script.js
@@ -1,47 +1,21 @@
-// Sets important constants and variables
-
-const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
 
 
-let output = document.getElementById("myRange");
+//////////////////////////////////////////////////////////////
 
-console.log(output.value);
+let container = document.querySelector(".container");
+let size = parseInt(window.getComputedStyle(container).width, 10)
 
-// Creates a default grid sized 16x16
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
+function createGrid(gridSize) {
+  for (let i = 0; i < gridSize ** 2; i++) {
+    let gridBlock = document.createElement("div");
+  
+    gridBlock.classList.add("box");
+
+    gridBlock.addEventListener("mouseover", changeGridColor);
+    container.appendChild(gridBlock);
+  }
 }
 
-// Takes (rows, columns) input and makes a grid
-function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
-      let cell = document.createElement("div");
+function changeGridColor() {}
 
-      cell.addEventListener("click", () => {
-        cell.style.setProperty("background-color", "black");
-    });
-      
-      container.appendChild(cell).className = "grid-item";
-    };
-  };
-
-// Creates columns
-function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-        };
-
-    };
-};
-
-makeRows(8,8);
-
-
-
+createGrid(16);
