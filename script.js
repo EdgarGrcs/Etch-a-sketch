e043f464653bf9e77ff348c9d7a8b7bd553bdbb6
EdgarGrcs <edgar.gricius2@gmail.com>
2022-03-13 22:43:34 +0100

    add userInput, remove slider, add changable grid sizes

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 
 const container = document.querySelector(".container");
 const clearButton = document.querySelector(".clear-button");
-const slider = document.querySelector(".range_slider");
 const rgbButton = document.querySelector(".rgb_button");
+const userInput = document.getElementById("quantity");
 
 
 
@@ -13,37 +13,58 @@ function createGrid (gridSize = 16){
   for (let i = 0; i < gridSize ** 2; i++){
     let columnRow = document.createElement("div");
     columnRow.classList.add("box");
-    columnRow.addEventListener("mouseover", changeGridColor);
+    columnRow.addEventListener("mouseover", gridColor);
+    container.appendChild(columnRow);
+  }
+}
+
+rgbButton.addEventListener("change", changeGridColor);
+
+
+
+function changeGridColor(e){
+  let color = `rgb(${[0, 0, 0].map(channel => {
+    return Math.floor(Math.random() * 256);
+  }).join(',')})`
+  Object.assign(e.target.style, {
+    backgroundColor : color
+  });
+}
+
+
+
+
+
+
+function updateGrid()  {
+  container.innerHTML = "";
+  container.style.setProperty(
+      "grid-template-columns",`repeat(${userInput.value}, 2fr)`
+  );
+  container.style.setProperty(
+      "grid-template-rows",
+      `repeat(${userInput.value}, 2fr)`
+  );
+  for (let i = 0; i < userInput.value * userInput.value; i++){
+    let columnRow = document.createElement("div");
+    columnRow.classList.add("box");
+    columnRow.addEventListener("mouseover",gridColor);
     container.appendChild(columnRow);
   }
 }
 
 
+userInput.addEventListener("change", updateGrid);
 
-function changeGridColor(e) {
 
-  if (setRainbow === true){
-    e.target.style.backGroundColor = "rgba(255,255,255,0)";
-  }
-
+function gridColor(e) {
   e.target.style.backgroundColor = "grey";
-
 }
 
-rgbButton.addEventListener("click",setRainbow);
+
 clearButton.addEventListener("click", clearEverything);
 
-let rainbow = false;
-function setRainbow () {
 
-  if (rainbow === false){
-    return true;
-  }
-  if (rainbow === true){
-    return false;
-  }
-
-}
 
 
 function clearEverything () {
